1957fa152ab7ec59add7c176616082840e07f97d
Victor Selegean <victor.selegean@garmin.com>
2025-06-06 15:37:42 +0300

    Initial Setup
    Change-Id: I8ac06caf93b91df039cf9921115ff3e137961384

---
+++ app/src/main/java/ubb/victors3136/epigraphmobile/MainActivity.kt
@@ -0,0 +1,47 @@
+package ubb.victors3136.epigraphmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ubb.victors3136.epigraphmobile.ui.theme.EpigraphMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EpigraphMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EpigraphMobileTheme {
+        Greeting("Android")
+    }
+}
