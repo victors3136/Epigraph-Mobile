a9831a243daefcca62a7a86958fe4d3b186b6307
Victor Selegean <victor.selegean@garmin.com>
2025-06-07 23:01:28 +0300

    Improve form styling
    Change-Id: Ic9370a4bcd927b26c0af3ba34f3dc23ebbe9858f

--- app/src/main/java/ubb/victors3136/epigraphmobile/MainActivity.kt
+++ app/src/main/java/ubb/victors3136/epigraphmobile/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -26,6 +27,7 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
@@ -44,6 +46,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -271,16 +274,19 @@ fun EpigraphTextField(value: String, label: String, onChange: (String) -> Unit) 
     OutlinedTextField(
         value = value,
         onValueChange = { onChange(it) },
-        label = { EpigraphTextBox(label, primary = false) },
-        modifier = Modifier.fillMaxWidth(),
+        label = { EpigraphTextBox(label) },
         textStyle = MaterialTheme.typography.bodyMedium.copy(
             color = ThemeProvider.get().primaryText()
         ),
         colors = OutlinedTextFieldDefaults.colors(
             focusedTextColor = ThemeProvider.get().primaryText(),
-            unfocusedTextColor = ThemeProvider.get().secondaryText(),
             focusedBorderColor = ThemeProvider.get().primaryAccent(),
+            focusedLabelColor =  ThemeProvider.get().primaryAccent(),
+            focusedPlaceholderColor =  ThemeProvider.get().primaryText(),
+            unfocusedTextColor = ThemeProvider.get().secondaryText(),
             unfocusedBorderColor = ThemeProvider.get().secondaryAccent(),
+            unfocusedLabelColor =  ThemeProvider.get().secondaryAccent(),
+            unfocusedPlaceholderColor = ThemeProvider.get().secondaryText(),
             cursorColor = ThemeProvider.get().primaryAccent(),
 
             )
